108202325a0511d1a6bde8a82f3459fa8d1307e0
Producer234 <prexplanation2@gmail.com>
2025-12-05 18:10:44 +0600

    Remove unused git import and call
    Removed unused import and function call for 'git'.

--- DeadlineTech/__init__.py
+++ DeadlineTech/__init__.py
@@ -1,14 +1,12 @@
 # Powered By Team DeadlineTech
 from DeadlineTech.core.bot import Anony
 from DeadlineTech.core.dir import dirr
-from DeadlineTech.core.git import git
 from DeadlineTech.core.userbot import Userbot
 from DeadlineTech.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
-git()
 dbb()
 heroku()
 
